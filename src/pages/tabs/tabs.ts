b37d0fa31ad269770d0b777369250033f40a92d4
Ramon Figuera <refm.130995@gmail.com>
2019-01-24 00:07:01 -0400

    Tabs Inferiores.

---
+++ src/pages/tabs/tabs.ts
@@ -0,0 +1,25 @@
+import { Component } from '@angular/core';
+import { IonicPage, NavController } from 'ionic-angular';
+
+/**
+ * Generated class for the TabsPage tabs.
+ *
+ * See https://ionicframework.com/docs/components/#navigation for more info on
+ * Ionic pages and navigation.
+ */
+
+@IonicPage()
+@Component({
+  selector: 'page-tabs',
+  templateUrl: 'tabs.html'
+})
+export class TabsPage {
+
+  chatRoot = 'ChatPage'
+  eventsRoot = 'EventsPage'
+  settingsRoot = 'SettingsPage'
+
+
+  constructor(public navCtrl: NavController) {}
+
+}
