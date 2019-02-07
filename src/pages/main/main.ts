38b3ac055cac028888810baa3fd95dde79149be5
mgarreaza13 <mg.arreaza.13@gmail.com>
2019-02-07 17:12:03 -0400

    mgarreaza13 - navegacion de nuevo funcionando

--- src/pages/main/main.ts
+++ src/pages/main/main.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { LoginPage } from './../login/login';
+import { SignupPage } from '../signup/signup';
 
 /**
  * Generated class for the MainPage page.
@@ -18,6 +20,12 @@ export class MainPage {
   constructor(public navCtrl: NavController, public navParams: NavParams) {
   }
 
+  gotoLogin(){
+    this.navCtrl.setRoot(LoginPage)
+  }
+  goToRegister(){
+    this.navCtrl.setRoot(SignupPage)
+  }
   ionViewDidLoad() {
     console.log('ionViewDidLoad MainPage');
   }
