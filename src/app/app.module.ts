5b0ef93c381886723dd71695488ff2a5da47e28f
jose luis torres loria <joseu0202@gmail.com>
2019-03-26 23:56:32 -0500

    Geolocation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,11 @@ import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
 import { ChatPrivatePage } from '../pages/chat-private/chat-private';
+// Comandos
+//$ ionic cordova plugin add cordova-plugin-geolocation --variable GEOLOCATION_USAGE_DESCRIPTION="To locate you"
+//$ npm install --save @ionic-native/geolocation@4
+//GeoLocation
+import { Geolocation } from '@ionic-native/geolocation';
 
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
@@ -45,6 +50,8 @@ import { TabsPage } from '../pages/tabs/tabs';
   ],
   providers: [
     StatusBar,
+    //Importando GeoLocation
+    Geolocation,
     SplashScreen,
     {provide: ErrorHandler, useClass: IonicErrorHandler}
   ]
--- src/pages/events/events.ts
+++ src/pages/events/events.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 
+import { Geolocation } from '@ionic-native/geolocation';
+
 /**
  * Generated class for the EventsPage page.
  *
@@ -14,12 +16,29 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
   templateUrl: 'events.html',
 })
 export class EventsPage {
+//Declarando latitude y longitude
+latitude:number;
+longitude:number;
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+
+  constructor(public navCtrl: NavController, public navParams: NavParams,
+  	private geolocation: Geolocation) {
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad EventsPage');
   }
+
+  getLocation(){
+
+this.geolocation.getCurrentPosition().then((resp) => {
+this.latitude=resp.coords.latitude;
+this.longitude=resp.coords.longitude
+
+}).catch((error) => {
+console.log('Error getting location', error);
+});
+
+}
 
 }
